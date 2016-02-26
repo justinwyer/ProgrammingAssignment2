2a2a34d76a791938ce0479868f28dd6ae4d23f13
Justin Wyer <justin@lifeasageek.com>
2016-02-26 16:59:45 +0000

    Implement a cachable matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## create a cacheable matrix and cache the inverse of the matrix using google's
+## R code style, except for the function names the function names would be
+## MakeCacheMatrix & CacheSolve but that would break the assignment's contract
+## I would have chosen MakeCachableMatrix & SolveCachableMatrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## accept or create a new matrix and add functions to get or set the inverse
+makeCacheMatrix <- function(unwrappedMatrix = matrix()) {
+  inverseMatrix <- NULL
+  ## set the matrix and clear the cache
+  Set <- function(newMatrix) {
+    unwrappedMatrix <<- newMatrix
+    inverseMatrix <<- NULL
+  }
+  Get <- function() unwrappedMatrix
+  SetInverse <- function(newInverseMatrix) inverseMatrix <<- newInverseMatrix
+  GetInverse <- function() inverseMatrix
+  return(list(Set = Set, Get = Get,
+       SetInverse = SetInverse,
+       GetInverse = GetInverse))
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return the cached inverse or solve, cache and return the inverse
+cacheSolve <- function(cachedMatrix) {
+  inverseMatrix <- cachedMatrix$GetInverse()
+  if(!is.null(inverseMatrix)) {
+    message("using cache")
+    return(inverseMatrix)
+  } else {
+    unwrappedMatrix <- cachedMatrix$Get()
+    inverseMatrix <- solve(unwrappedMatrix)
+    cachedMatrix$SetInverse(inverseMatrix)
+    return(inverseMatrix)
+  }
 }
